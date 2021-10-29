4c71ef1609d0c0bc3a3360895688aa1dfd44580f
Syed Shahriyar Ali <shahriyarali08@gmail.com>
2021-10-30 02:54:54 +0500

    Android API Changes

--- routes/android.js
+++ routes/android.js
@@ -1,27 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const Campgroud = require('../Models/Campgroud');
-const catchAsync = require('../Utils/catchAsync');
-const { isLoggedIn } = require('../middleware-android');
+const passport = require("passport");
+const Campgroud = require("../Models/Campgroud");
+const catchAsync = require("../Utils/catchAsync");
+const { isLoggedIn } = require("../middleware-android");
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
-    res.setHeader("Access-Control-Expose-Headers", "*")
-    res.send("Authenticated !");    
-})
+router.post("/login", passport.authenticate("local"), (req, res) => {
+  res.send(JSON.stringify(req.user));
+});
 
-router.get('/', isLoggedIn, catchAsync(async (req, res) => {
-    const campgrounds = await Campgroud.find();
+router.get(
+  "/",
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campgroud.find({}).sort({ _id: -1 });
     res.send(JSON.stringify(campgrounds));
-}));
+  })
+);
 
-router.get('/session', isLoggedIn, catchAsync(async (req, res) => {
-    res.send('Authorized')
-}));
+router.get(
+  "/session",
+  isLoggedIn,
+  catchAsync(async (req, res) => {
+    res.send("Authorized");
+  })
+);
 
-router.get('/logout', (req, res) => {
-    req.logout();
-    res.send('Logged Out');
-})
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.send("Logged Out");
+});
 
-module.exports = router
+module.exports = router;
